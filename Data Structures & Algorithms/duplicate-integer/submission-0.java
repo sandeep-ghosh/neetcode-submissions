7124248bd8eac27fa28c28bbc9161f53383a50ca
Sandeep Ghosh <36575836+sandeep-ghosh@users.noreply.github.com>
2026-05-30 22:37:39 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,16 @@
+
+
+class Solution {
+    
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i=0;i<nums.length;i++){
+            hs.add(nums[i]);
+        }
+        if (hs.size() == nums.length){
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
