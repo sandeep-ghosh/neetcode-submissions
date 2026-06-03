96b3bdb84ff2d534ceb81edc6c3dbc68354c73a3
Sandeep Ghosh <36575836+sandeep-ghosh@users.noreply.github.com>
2026-06-03 13:27:52 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> result = new HashMap<>();
+        for (String s : strs) {
+            int[] alpha = new int[26];
+            for (char c : s.toCharArray()) {
+                int index = c - 'a';
+                alpha[index] += 1;
+            }
+            // System.out.println(Arrays.toString(alpha));
+            //  use the array as a key of hashmap
+            String arrstr = Arrays.toString(alpha);
+            result.putIfAbsent(arrstr, new ArrayList<String>());
+            result.get(arrstr).add(s);
+        }
+
+        return new ArrayList<List<String>>(result.values());
+    }
+}
