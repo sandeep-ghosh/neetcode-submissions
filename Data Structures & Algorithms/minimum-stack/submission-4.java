f82be9f99528dec98d4bc75f3270412636200e08
Sandeep Ghosh <36575836+sandeep-ghosh@users.noreply.github.com>
2026-06-06 21:08:36 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,30 @@
+class MinStack {
+    List<Integer> arr, min_arr;
+    public MinStack() {
+        arr = new ArrayList<>();
+        min_arr = new ArrayList<>();
+    }
+
+    public void push(int val) {
+        arr.add(val);
+        if (min_arr.isEmpty()) {
+            min_arr.add(val);
+        } else {
+            min_arr.add(Math.min(val, min_arr.get(min_arr.size() - 1)));
+        }
+    }
+
+    public void pop() {
+        arr.remove(arr.size() - 1);
+        min_arr.remove(min_arr.size() - 1);
+    }
+
+    public int top() {
+        int ret = arr.get(arr.size() - 1);
+        return ret;
+    }
+
+    public int getMin() {
+        return min_arr.get(min_arr.size() - 1);
+    }
+}
