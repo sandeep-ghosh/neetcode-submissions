c449502307d8deaeeccd55d2f578fb76a5ab17c1
Sandeep Ghosh <36575836+sandeep-ghosh@users.noreply.github.com>
2026-06-08 12:02:30 -0400

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String encode(List<String> strs) {
+        // String result = "";
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            // result = result + s.length() + '#' + s;
+            sb.append(s.length()).append('#').append(s);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<>();
+        if (str.length() == 0)
+            return list;
+        int i = 0;
+        while (i < str.length()) {
+            int r = str.indexOf('#', i);
+            int length = Integer.parseInt(str.substring(i, r));
+            list.add(str.substring(r + 1, r + 1 + length));
+            i = r + 1 + length;
+        }
+        return list;
+    }
+}
